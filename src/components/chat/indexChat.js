3b4efdb94b2292715c21c87aaf0185caaa96297e
TBS093A <zukkamil.44@gmail.com>
2020-03-11 18:09:25 +0100

    Add ready chat style

--- src/components/chat/indexChat.js
+++ src/components/chat/indexChat.js
@@ -15,7 +15,34 @@ const IndexChat = ({ user }) => {
                     chat
             </div>
             <div className='chatBody'>
-                
+                <div className='lobbyMessage'>
+                    <form>
+                        <input placeholder='lobby / room name'/>
+                        <button type='submit'>Connect</button>
+                    </form>
+                </div>
+                <div className='listMessage'>
+                    <div className='rowMessage'>
+                        <div className="chatAvatar">
+                        </div>
+                        <div className='userMessage'>
+                            niesamowite naprwdę nie wierygodne
+                        </div>
+                    </div>
+                    <div className='rowMessage'>
+                        <div className="chatAvatar">
+                        </div>
+                        <div className='otherMessage'>
+                            xd
+                        </div>
+                    </div>
+                </div>
+                <div className='textMessage'>
+                    <form>
+                        <input placeholder='message'/>
+                        <button type='submit'>Send</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
